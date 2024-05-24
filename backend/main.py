5c087cd119ac4bc71ae68cda28b66d8a0285381a
Nadegda0909 <55895472@mail.ru>
2024-05-24 22:06:58 +0300

    Теперь все правильно на серваке развертывается

--- backend/main.py
+++ backend/main.py
@@ -12,18 +12,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class LoginRequest(BaseModel):
     username: str
     password: str
+
 
 @app.get("/api/data")
 def read_root():
     return {"message": "Я не панк"}
 
+
 @app.post("/api/button-click")
 def button_click():
     print("Button was clicked")
     return {"status": "Button click received"}
+
 
 @app.post("/api/login")
 def login(request: LoginRequest):
