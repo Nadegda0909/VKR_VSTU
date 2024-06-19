2545c69ff1178b6c886cd01d645c1e5a11836eaa
Nadegda0909 <55895472@mail.ru>
2024-06-19 07:09:35 +0300

    Теперь перед загрузкой файлы с расписанием удаляются

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
+import os
+import shutil
+import asyncio
+import json
+import uuid
 from fastapi import FastAPI, HTTPException, Request, Response, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from pydantic import BaseModel
 from sse_starlette.sse import EventSourceResponse
 from backend.work_with_excel_rasp.downloader import *
-import asyncio
-import json
-import uuid
 
 app = FastAPI()
 
@@ -22,18 +24,22 @@ app.add_middleware(SessionMiddleware, secret_key="!secret")
 
 app.state.session_store = {}
 
+
 class LoginRequest(BaseModel):
     username: str
     password: str
+
 
 @app.get("/api/data")
 def read_root():
     return {"message": "Тестовое сообщение!"}
 
+
 @app.post("/api/button-click")
 def button_click():
     print("Button was clicked")
     return {"status": "Button click received"}
+
 
 @app.post("/api/login")
 def login(request: LoginRequest):
@@ -42,7 +48,21 @@ def login(request: LoginRequest):
     else:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
+
+def delete_dirs_in_path(path: str):
+    items = os.listdir(path=path)
+    folders = [item for item in items if os.path.isdir(os.path.join(path, item)) and "__" not in item]
+    for folder in folders:
+        folder_path = os.path.join(path, folder)
+        try:
+            shutil.rmtree(folder_path)
+            print(f'{Fore.GREEN}Папка {folder_path} успешно удалена{Style.RESET_ALL}')
+        except OSError as e:
+            print(f'{Fore.RED}Ошибка при удалении папки {folder_path}: {e}{Style.RESET_ALL}')
+
+
 async def download_and_convert_schedule(session_id: str):
+    await asyncio.to_thread(delete_dirs_in_path, path='work_with_excel_rasp')
     session = json.loads(app.state.session_store.get(session_id, '{}'))
     session['progress'] = 1
     app.state.session_store[session_id] = json.dumps(session)
@@ -63,6 +83,7 @@ async def download_and_convert_schedule(session_id: str):
     app.state.session_store[session_id] = json.dumps(session)
     yield {"event": "message", "data": "done"}
 
+
 @app.get("/api/download_rasp")
 async def download_rasp(request: Request):
     session_id = request.cookies.get("session")
@@ -73,6 +94,7 @@ async def download_rasp(request: Request):
         app.state.session_store[session_id] = json.dumps({"progress": 0})
         return response
     return EventSourceResponse(download_and_convert_schedule(session_id))
+
 
 @app.get("/api/progress")
 def get_progress(request: Request):
